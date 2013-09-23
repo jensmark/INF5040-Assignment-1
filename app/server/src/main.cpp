563d4d99f6e17383e7d75eba0625ddeca21aa5ce
Jens Kristoffer Reitan Markussen <jenskristoffer@ubuntu.(none)>
2013-09-23 14:01:28 +0200

    some more server work

--- app/server/src/main.cpp
+++ app/server/src/main.cpp
@@ -28,18 +28,20 @@ int main(int argc, char** argv) {
 		CORBA::Object_var SA_obj = server->_this();
 		
 		CORBA::String_var sior(orb->object_to_string(SA_obj.in()));
-		std::cerr << "'" << (char*)sior << "'" << std::endl;
+		//std::cerr << "'" << (char*)sior << "'" << std::endl;
 		
 		
-		CORBA::Object_var obj1=orb->resolve_initial_references("OmniNameService");
+		CORBA::Object_var obj1=orb->resolve_initial_references("NameService");
 		assert(!CORBA::is_nil(obj1.in()));
 		
 		CosNaming::NamingContext_var nc = CosNaming::NamingContext::_narrow(obj1.in());
 		assert(!CORBA::is_nil(nc.in()));
 		
+		std::cout << "Binding service 'QuizService'" << std::endl;
+		
 		CosNaming::Name name;
 		name.length(1);
-		name[0].id=CORBA::string_dup("DataServiceName1");
+		name[0].id=CORBA::string_dup("QuizService");
 		nc->rebind (name,SA_obj.in());
 		
 		server->_remove_ref();
@@ -47,7 +49,10 @@ int main(int argc, char** argv) {
 		PortableServer::POAManager_var pmgr = _poa->the_POAManager();
 		pmgr->activate();
 		
+		std::cout << "Server is running..." << std::endl;
 		orb->run();
+		std::cout << "Server is shutting down.." << std::endl;
+		
 		orb->destroy();
 		
 		free(name[0].id);
--- app/server/src/quiz_server.cpp
+++ app/server/src/quiz_server.cpp
@@ -1,21 +1,21 @@
-	#include "quiz_server.h"
-	
-	QuizServerImpl::QuizServerImpl(){
-	}
-	QuizServerImpl::~QuizServerImpl(){
-	}
-   
-	CORBA::Long QuizServerImpl::newQuestion(Quiz::CompleteQuestion* question){
-		return 0;
-	}
-    CORBA::Boolean QuizServerImpl::getRandomQuestion(Quiz::Question_out randomQuestion){
-    	return true;
-	}
-    CORBA::Boolean QuizServerImpl::answerQuestion(CORBA::Long questionId,
-    				const Quiz::QuizServer::alternativesIds& answer, 
-    				Quiz::QuizServer::alternativesIds_out correct){
-    	return true;
-	}
-    CORBA::Long QuizServerImpl::removeQuestion(CORBA::Long questionId){
-    	return 0;
-	}
+#include "quiz_server.h"
+
+QuizServerImpl::QuizServerImpl(){
+}
+QuizServerImpl::~QuizServerImpl(){
+}
+
+CORBA::Long QuizServerImpl::newQuestion(Quiz::CompleteQuestion* question){
+	return 0;
+}
+CORBA::Boolean QuizServerImpl::getRandomQuestion(Quiz::Question_out randomQuestion){
+	return true;
+}
+CORBA::Boolean QuizServerImpl::answerQuestion(CORBA::Long questionId,
+				const Quiz::QuizServer::alternativesIds& answer, 
+				Quiz::QuizServer::alternativesIds_out correct){
+	return true;
+}
+CORBA::Long QuizServerImpl::removeQuestion(CORBA::Long questionId){
+	return 0;
+}
