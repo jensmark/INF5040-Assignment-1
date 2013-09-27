6e1c3747c6613e94ac22259b3c42e77c8eb25afa
kriskars <kriskars@kriskars-VirtualBox.(none)>
2013-09-27 13:48:13 +0200

    implemented most of the functionality in quiz_server

--- app/server/src/quiz_server.cpp
+++ app/server/src/quiz_server.cpp
@@ -1,21 +1,45 @@
 #include "quiz_server.h"
-
+#include <iostream>
+#include <stdlib.h>
 QuizServerImpl::QuizServerImpl(){
+	srand(time(NULL));
 }
 QuizServerImpl::~QuizServerImpl(){
 }
 
 CORBA::Long QuizServerImpl::newQuestion(Quiz::CompleteQuestion* question){
-	return 0;
+	std::cout << question->sentence() << std::endl;
+	_completeQuestions[_completeQuestions.size()] = question;
+	return _completeQuestions.size()-1;
 }
+
 CORBA::Boolean QuizServerImpl::getRandomQuestion(Quiz::Question_out randomQuestion){
+	if(_completeQuestions.empty())
+		return false;
+
+	Quiz::CompleteQuestion* q = _completeQuestions[_randNumber(0, _completeQuestions.size()-1)];
+	randomQuestion = q;
 	return true;
 }
+
 CORBA::Boolean QuizServerImpl::answerQuestion(CORBA::Long questionId,
 				const Quiz::QuizServer::alternativesIds& answer, 
 				Quiz::QuizServer::alternativesIds_out correct){
 	return true;
 }
+
 CORBA::Long QuizServerImpl::removeQuestion(CORBA::Long questionId){
-	return 1234567;
+	std::map<CORBA::Long, Quiz::CompleteQuestion*>::iterator it;
+	it = _completeQuestions.find(questionId);
+	if(it == _completeQuestions.end()){
+		return -1;
+	}
+	else{
+		return questionId;
+	}
 }
+
+int QuizServerImpl::_randNumber(int low, int high){
+	 return ((int(rand()) / int(high)) * (high - low)) + low;
+}
+
